44ef8ca52980c699bc7aca5a294ce0dcfce9ffa1
Tom Owen <tom-tom_1989@hotmail.com>
2015-03-31 21:32:36 +1100

    Level 1
    added Level 1

---
+++ level1.js
@@ -0,0 +1,74 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var tileset = document.createElement("img");
+tileset.src = level1.tileset[0].image;
+
+var LAYER_COUNT = level1.layers.length;
+var TILESET_COUNT_X = 14;
+var TILESET_COUNT_Y = 14;
+
+var TILE = level1.tilewidth;
+var TILESET_TILE = level1.tilesets[0].tilewidth;
+var TILESET_SPACING = level1.tilesets[0].spacing;
+var TILESET_MARGIN = level1.tilesets[0].margin;
